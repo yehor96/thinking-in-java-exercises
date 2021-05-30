a0c8d7f2fff9b6ca8fc6a20113606c5da697c537
yehor96 <yehor.shyriaiev@gmail.com>
2021-05-30 18:55:26 +0300

    strings ex20

---
+++ src/main/java/com/chapters/strings/ex20/Main.java
@@ -0,0 +1,10 @@
+package com.chapters.strings.ex20;
+
+public class Main {
+    public static void main(String[] args) {
+        Container container1 = new Container("5 5.1 6 5.2 text");
+        Container container2 = new Container("11 0.9234 11234235 1.2025245623 osmeetoatGFJdfjgfd  df 3");
+        System.out.println(container1);
+        System.out.println(container2);
+    }
+}
