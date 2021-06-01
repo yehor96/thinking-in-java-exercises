d6e9f2f86e1dbc0f073c06d1d55e4c7001764cdb
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-01 14:30:16 +0300

    typeinfo ex4

--- src/main/java/com/chapters/typeinfo/ex3_ex4/Shapes.java
+++ src/main/java/com/chapters/typeinfo/ex3_ex4/Shapes.java
@@ -1,7 +1,6 @@
-package com.chapters.typeinfo.ex3;
+package com.chapters.typeinfo.ex3_ex4;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
