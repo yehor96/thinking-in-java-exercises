e787b33af214384bac01070fb35e3c330d307dbc
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-01 14:28:01 +0300

    typeinfo ex3

---
+++ src/main/java/com/chapters/typeinfo/ex3/Shapes.java
@@ -0,0 +1,55 @@
+package com.chapters.typeinfo.ex3;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    public String toString() {
+        return "Triangle";
+    }
+}
+
+class Rhomboid extends Shape {
+    public String toString() {
+        return "Rhomboid";
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        List<Shape> shapeList = Arrays.asList(new Circle(), new Square(), new Triangle(), new Rhomboid());
+        Collections.shuffle(shapeList);
+        for (Shape shape : shapeList) {
+            shape.draw();
+            if(shape instanceof Rhomboid) {
+                Rhomboid rhomboid = (Rhomboid) shape;
+                System.out.println("Downcasted to Rhomboid - " + rhomboid);
+                // To Circle:
+                // Circle circle = (Circle) shape;
+                // System.out.println("Downcasted to Circle - " + circle);
+            }
+        }
+    }
+}
