4ad5f9310625d7eb7e08b50df66ccf0e90bc5b5b
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-10 10:01:47 +0300

    generics ex1, ex2

---
+++ src/main/java/com/chapters/generics/ex1/Holder3.java
@@ -0,0 +1,35 @@
+package com.chapters.generics.ex1;
+
+public class Holder3<T> {
+    private T a;
+
+    public Holder3(T a) {
+        this.a = a;
+    }
+
+    public void set(T a) {
+        this.a = a;
+    }
+
+    public T get() {
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Holder3<Automobile> h3 = new Holder3<>(new Automobile());
+        Automobile a = h3.get();
+        System.out.println(a);
+
+        h3.set(new Juk());
+        Juk j = (Juk) h3.get();
+        System.out.println(j);
+    }
+}
+
+class Automobile{
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
+class Juk extends Automobile{}
