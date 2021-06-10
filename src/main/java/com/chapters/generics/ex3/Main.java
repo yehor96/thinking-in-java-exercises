5dcfdaf264d8bb8c11d0aa65bd209660450404e4
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-10 21:26:44 +0300

    generics ex3, ex4

---
+++ src/main/java/com/chapters/generics/ex3/Main.java
@@ -0,0 +1,11 @@
+package com.chapters.generics.ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        SixTuple<String, String, String, String, String, Integer> tuple =
+                new SixTuple<>("First", "Second", "Third", "Fourth", "Five", 6);
+
+        System.out.println(tuple);
+
+    }
+}
