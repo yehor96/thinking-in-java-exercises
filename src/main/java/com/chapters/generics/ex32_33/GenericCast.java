3b3de158375c8c4b62f188f1a4a63e23151c96be
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-21 18:29:31 +0300

    generics ex32, ex33

---
+++ src/main/java/com/chapters/generics/ex32_33/GenericCast.java
@@ -0,0 +1,46 @@
+package com.chapters.generics.ex32_33;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FixedSizeStack<T> {
+    private int index = 0;
+    private int size;
+    private List<Object> storage;
+
+    public FixedSizeStack(int size) {
+        this.size = size;
+        storage = new ArrayList<>();
+    }
+
+    public void push(T item) {
+        if (size < ++index) {
+            throw new RuntimeException("FixedSizeStack storage has been overflowed.");
+        }
+
+        storage.add(item);
+    }
+
+    @SuppressWarnings("unchecked")
+    public T pop() {
+        if (0 > --index) {
+            throw new RuntimeException("FixedSizeStack storage has been underflowed.");
+        }
+
+        return (T) storage.get(index);
+    }
+}
+
+public class GenericCast {
+    public static final int SIZE = 1;
+
+    public static void main(String[] args) {
+        FixedSizeStack<String> strings = new FixedSizeStack<>(SIZE);
+        for (String s : "A B C D E F G H I J".split(" "))
+            strings.push(s);
+        for (int i = 0; i < SIZE; i++) {
+            String s = strings.pop();
+            System.out.print(s + " ");
+        }
+    }
+}
