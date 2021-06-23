1cf8459decb157c4341706f181d4a237b226ada4
yehor96 <yehor.shyriaiev@gmail.com>
2021-06-23 09:25:37 +0300

    generics ex34

---
+++ src/main/java/com/chapters/generics/ex34/Main.java
@@ -0,0 +1,8 @@
+package com.chapters.generics.ex34;
+
+public class Main {
+    public static void main(String[] args) {
+        var m = new ImplClass();
+        m.implMethod();
+    }
+}
---
+++ src/main/java/com/chapters/generics/ex34/SelfBoundClass.java
@@ -0,0 +1,11 @@
+package com.chapters.generics.ex34;
+
+public abstract class SelfBoundClass<T extends SelfBoundClass<T>> {
+
+    protected abstract T abstractMethod(T value);
+
+    public T implMethod() {
+        return abstractMethod(null);
+    }
+
+}
