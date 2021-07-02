e604a6faee44353122784344ca388e862a42cd2c
yehor96 <yehor.shyriaiev@gmail.com>
2021-07-02 10:01:26 +0300

    arrays ex12, ex13

---
+++ src/main/java/com/chapters/arrays/ex12_13/Main.java
@@ -0,0 +1,26 @@
+package com.chapters.arrays.ex12_13;
+
+public class Main {
+
+    public static void main(java.lang.String[] args) {
+        generateDoubles();
+        generateString();
+    }
+
+    public static void generateDoubles() {
+        var generator = new CountingGenerator.Double();
+        for (int i = 0; i < 10; i++) {
+            System.out.print(generator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    public static void generateString() {
+        var generator = new CountingGenerator.Character();
+        StringBuilder sb = new StringBuilder(generator.next());
+        for (int i = 0; i < 9; i++) {
+            sb.append(generator.next());
+        }
+        System.out.println(sb.toString());
+    }
+}
