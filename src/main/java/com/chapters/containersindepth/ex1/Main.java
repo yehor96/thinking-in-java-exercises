190821fec652de10ab3758587d800139f89cb546
yehor96 <yehor.shyriaiev@gmail.com>
2021-07-07 11:51:04 +0300

    containersindepth ex1, ex2

---
+++ src/main/java/com/chapters/containersindepth/ex1/Main.java
@@ -0,0 +1,28 @@
+package com.chapters.containersindepth.ex1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>(Countries.names(6));
+        List<String> linkedList = new LinkedList<>(Countries.names(6));
+
+        Collections.sort(arrayList);
+        Collections.sort(linkedList);
+        System.out.println(arrayList);
+        System.out.println(linkedList);
+
+        Collections.shuffle(arrayList);
+        Collections.shuffle(linkedList);
+        System.out.println(arrayList);
+        System.out.println(linkedList);
+
+        Collections.shuffle(arrayList);
+        Collections.shuffle(linkedList);
+        System.out.println(arrayList);
+        System.out.println(linkedList);
+    }
+}
---
+++ src/main/java/com/chapters/containersindepth/ex2/Main.java
@@ -0,0 +1,26 @@
+package com.chapters.containersindepth.ex2;
+
+import com.chapters.containersindepth.ex1.Countries;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
+
+public class Main {
+    public static void main(String[] args) {
+        char letter = 'N';
+
+        Set<String> countrySet = Countries.names().stream()
+                .filter(c -> c.startsWith(Character.toString(letter)))
+                .collect(Collectors.toSet());
+
+        Map<String, String> countryMap = Countries.capitals().entrySet().stream()
+                .filter(c -> c.getKey().startsWith(Character.toString(letter)))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        System.out.println(countrySet);
+        System.out.println(countryMap);
+    }
+}
