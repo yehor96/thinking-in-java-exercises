da6c8f3e694897f8ac4c10924563a73584f5f3fd
yehor96 <yehor.shyriaiev@gmail.com>
2021-07-22 20:08:57 +0300

    containersindepth ex26, ex27

--- src/main/java/com/exercises/chapters/containersindepth/ex26/CountedString.java
+++ src/main/java/com/exercises/chapters/containersindepth/ex26/CountedString.java
@@ -1,4 +1,54 @@
 package com.exercises.chapters.containersindepth.ex26;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountedString {
+    private static List<String> created = new ArrayList<>();
+    private String s;
+    private char c;
+    private int id = 0;
+
+    public CountedString(String str, char c) {
+        this.s = str;
+        this.c = c;
+        created.add(s);
+        for (String s2 : created)
+            if (s2.equals(s))
+                id++;
+    }
+
+    public String toString() {
+        return "String: " + s + " id: " + id + " char: " + c + " hashCode(): " + hashCode();
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + s.hashCode();
+        result = 37 * result + Character.hashCode(c);
+        result = 37 * result + id;
+        return result;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof CountedString &&
+                s.equals(((CountedString) o).s) &&
+                id == ((CountedString) o).id;
+    }
+
+    public static void main(String[] args) {
+        Map<CountedString, Integer> map = new HashMap<>();
+        CountedString[] cs = new CountedString[5];
+        for (int i = 0; i < cs.length; i++) {
+            cs[i] = new CountedString("hi", 'o');
+            map.put(cs[i], i);
+        }
+        System.out.println(map);
+        for (CountedString cstring : cs) {
+            System.out.println("Looking up " + cstring);
+            System.out.println(map.get(cstring));
+        }
+    }
 }
--- src/main/java/com/exercises/chapters/containersindepth/ex27/CountedString.java
+++ src/main/java/com/exercises/chapters/containersindepth/ex27/CountedString.java
@@ -1,4 +1,50 @@
 package com.exercises.chapters.containersindepth.ex27;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountedString {
+    private static List<String> created = new ArrayList<>();
+    private String s;
+    private int id = 0;
+
+    public CountedString(String str) {
+        this.s = str;
+        created.add(s);
+        for (String s2 : created)
+            if (s2.equals(s))
+                id++;
+    }
+
+    public String toString() {
+        return "String: " + s + " id: " + id + " hashCode(): " + hashCode();
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + s.hashCode();
+        return result;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof CountedString &&
+                s.equals(((CountedString) o).s) &&
+                id == ((CountedString) o).id;
+    }
+
+    public static void main(String[] args) {
+        Map<CountedString, Integer> map = new HashMap<>();
+        CountedString[] cs = new CountedString[5];
+        for (int i = 0; i < cs.length; i++) {
+            cs[i] = new CountedString("hi");
+            map.put(cs[i], i);
+        }
+        System.out.println(map);
+        for (CountedString cstring : cs) {
+            System.out.println("Looking up " + cstring);
+            System.out.println(map.get(cstring));
+        }
+    }
 }
